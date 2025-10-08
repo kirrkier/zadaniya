138b00e5c6eb20bbe99fe0fe4d1e2ace6378ff28
Мальцева Алина <m.alina06@yandex.ru>
2025-10-08 20:25:56 +0500

    задание 5

---
+++ zadanie5/zadanie5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = Calculate(2, 3) + Calculate(3, 5) + Calculate(5, 7);
+            Console.WriteLine(f);
+        } 
+        static double Calculate(double x, double y) =>
+            Math.Pow(Math.E, (- Math.Sqrt(x + Math.Pow(y, 2))));
+    }
+}
