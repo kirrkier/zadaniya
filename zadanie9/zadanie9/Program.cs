5fb75dcf682814806fb3b3ba16fc5607e522f7f9
Мальцева Алина <m.alina06@yandex.ru>
2025-10-29 18:37:52 +0500

    zadanie9

---
+++ zadanie9/zadanie9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение аргумента");
+
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine($"f({x:F2}) = {F(x):F2}");
+        }
+        static double F(double x)
+        {
+            if (x < -1)
+                return 1;
+            else if (x<= 1 && x>=-1)
+                return -x;
+            else 
+                return -1;
+                
+                    
+                    
+                    
+                    }
+    }
+}
