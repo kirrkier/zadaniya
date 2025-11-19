3246560ae3f145c036b72eb0f71377adb1622f09
Мальцева Алина <m.alina06@yandex.ru>
2025-11-19 15:06:25 +0500

    zadanie10

---
+++ zadanie10.1/zadanie10.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число n");
+            int n;
+            long sum = 0;
+
+            int.TryParse(Console.ReadLine(), out n);
+            {
+                if (n >= 1)
+                {
+                    for (int i = n; i <= 2 * n; i++) {
+                        sum += (long)i * i;
+                    }
+
+                    Console.WriteLine($"Значение выражения равно: {sum}" );
+
+                }
+                else
+                    Console.WriteLine("Введите корректное значение для n. n - натуральное число.");
+            }
+        }
+    }
+}
+ 
---
+++ zadanie10.4/zadanie10.4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("zadanie10.4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("zadanie10.4")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("09228489-ccb6-4581-8c36-8b5c86682220")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
