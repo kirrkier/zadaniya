b80861b619f93e2c8ec7e49593e15d11b3c3874d
Мальцева Алина <m.alina06@yandex.ru>
2026-05-31 20:54:36 +0500

    zadanie13

---
+++ LibraryReaders.UnitTest/LibraryReaders.UnitTest/ReaderUnitTests.cs
@@ -0,0 +1,71 @@
+using NUnit.Framework;
+using System;
+using LibraryReaders;
+
+namespace LibraryReaders.UnitTests
+{
+    [TestFixture]
+    public class ReaderUnitTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var reader = CreateTestReader();
+
+            Assert.That(reader.Name, Is.EqualTo("Иван"));
+
+            Assert.That(reader.Surname, Is.EqualTo("Петров"));
+
+            Assert.That(reader.ReaderCardNumber, Is.EqualTo("12345"));
+        }
+
+        [Test]
+        public void ReturnDateTest()
+        {
+            var reader = CreateTestReader();
+
+            DateTime expected = reader.IssueDate.AddDays(30);
+
+            Assert.That(reader.ReturnDate, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetInfoTest()
+        {
+            var reader = CreateTestReader();
+
+            string[] info = reader.GetInfo();
+
+            Assert.That(info.Length, Is.EqualTo(2));
+
+            Assert.That(info[0], Is.EqualTo("Иван Петров"));
+
+            Assert.That(info[1].Contains("12345"), Is.True);
+        }
+
+        private Reader CreateTestReader()
+        {
+            var reader =
+                new Reader(
+                    "Иван",
+                    "Петров",
+                    "12345");
+
+            reader.Books.Add("Война и мир");
+            reader.Books.Add("Преступление и наказание");
+            reader.Books.Add("Евгений Онегин");
+
+            reader.IssueDate =
+                    new DateTime(2025, 5, 1);
+
+            reader.LoanPeriod =
+                TimeSpan.FromDays(30);
+
+            reader.DepositAmount = 1000;
+
+            return reader;
+        }
+    }
+}
+
+
---
+++ LibraryReaders.UnitTest/LibraryReaders/Reader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryReaders
+{
+    public class Reader
+    {
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public readonly string ReaderCardNumber;
+
+
+        public List<string> Books { get; set; }
+
+        public DateTime IssueDate { get; set; }
+
+        public TimeSpan LoanPeriod { get; set; }
+
+        public decimal DepositAmount { get; set; }
+
+
+        public DateTime ReturnDate =>
+            IssueDate.Add(LoanPeriod);
+
+        public Reader(
+            string name,
+            string surname,
+            string readerCardNumber)
+        {
+            Name = name;
+            Surname = surname;
+            ReaderCardNumber = readerCardNumber;
+
+            Books = new List<string>();
+
+            IssueDate = DateTime.Now;
+
+            LoanPeriod = TimeSpan.FromDays(14);
+
+            DepositAmount = 0;
+        }
+
+        public virtual string[] GetInfo()
+        {
+            string[] info = new string[2];
+
+            info[0] =
+                $"{Name} {Surname}";
+
+            info[1] =
+                $"Билет № {ReaderCardNumber}. " +
+                $"Книг: {Books.Count}. " +
+                $"Дата выдачи: {IssueDate:d}. " +
+                $"Срок выдачи: {LoanPeriod.Days} дней. " +
+                $"Вернуть до: {ReturnDate:d}. " +
+                $"Залог: {DepositAmount} руб.";
+
+            return info;
+        }
+    }
+}
